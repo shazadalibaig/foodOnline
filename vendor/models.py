ea9d4779149ed2159f3128610dca643a31d5fdc0
shazad <shazadalibaig@gmail.com>
2023-02-13 13:41:48 +0530

    customer and vendor dashboard

--- vendor/models.py
+++ vendor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User,UserProfile
+from accounts.utils import send_notification_email
 
 # Create your models here.
 
@@ -14,5 +15,19 @@ class Vendor(models.Model):
     
     def __str__(self):
         return self.vendor_name
+    
+    def save(self,*args,**kwargs):
+        if self.pk is not None:
+            orig = Vendor.objects.get(pk=self.pk)
+            if orig.is_approved != self.is_approved:
+                email_template = 'accounts/emails/admin_approval_email.html'
+                context = {'user' : self.user, 'is_approved' : self.is_approved}
+                if self.is_approved == True:
+                    mail_subject = "Congratulations! Your restaurant got approved"
+                    send_notification_email(mail_subject,email_template,context)
+                else:
+                    mail_subject = "we are sorry you approval got rejected"
+                    send_notification_email(mail_subject,email_template,context)
+        return super(Vendor,self).save(*args,**kwargs)
     
     
